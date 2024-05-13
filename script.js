ecd6cc6cf78099e65a3f8552c6d6d5f85e6801b3
ArielGonzalez3 <elvisarielgonzalez@gmail.com>
2024-05-13 22:11:36 +0200

    solucion de problemas en codigo

--- script.js
+++ script.js
@@ -9,31 +9,46 @@ let isError = false;
 
 function cleanInputString(str) {
   // console.log("original string: ", str);
-  const regex = /[+-]/g;
-  return str.replace(regex, "");
+  const regex = /[+-\s]/g;
+  return str.replace(regex, '');
 }
 
 // console.log(cleanInputString("+-99"));
 
 function isInvalidInput(str) {
-  let regex = /\d+e\d+/i;
+  const regex = /\d+e\d+/i;
   return str.match(regex);
 }
 
 // console.log(isInvalidInput("1e3"));
 // console.log(isInvalidInput("10"));
 
-function addEntry(){
+
+function addEntry() {
   const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-  const entryNumber = targetInputContainer.querySelectorAll().length + 1;
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
   const HTMLString = `
   <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
-  <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name" />
-  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label> 
-  <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories" />
-  `;
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input
+    type="number"
+    min="0"
+    id="${entryDropdown.value}-${entryNumber}-calories"
+    placeholder="Calories"
+  />`;
 
-  targetInputContainer.innerHTML += HTMLString;
-} 
+  // targetInputContainer.innerHTML += HTMLString;
+  targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
+}
+
+function getCaloriesFromInputs(list){
+  let calories = 0;
+
+  for(const item of list) {
+    const currVal = cleanInputString(item.value);
+    const invalidInputMatch = isInvalidInput(currVal);
+  }
+}
 
 addEntryButton.addEventListener("click", addEntry);
